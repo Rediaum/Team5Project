4430332265dbf0c51e1b96a545727970326160a2
Rediaum <cog030522@naver.com>
2025-07-21 15:21:02 +0900

    Base Setting

---
+++ src/main/java/edu/sm/Team5ProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5ProjectApplication.class, args);
+	}
+
+}
