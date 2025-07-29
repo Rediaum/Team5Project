137c26ddc7b827736a2f55a6f20839a1b930fb0f
Rediaum <cog030522@naver.com>
2025-07-29 16:12:46 +0900

    Update AddressRepository.java

--- src/main/java/edu/sm/repository/AddressRepository.java
+++ src/main/java/edu/sm/repository/AddressRepository.java
@@ -3,9 +3,11 @@ package edu.sm.repository;
 import edu.sm.dto.Address;
 import edu.sm.frame.ProjectRepository;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 @Mapper
 public interface AddressRepository extends ProjectRepository<Address, Integer> {
 
